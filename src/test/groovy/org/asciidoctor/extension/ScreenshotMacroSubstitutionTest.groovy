8d86394a2466d722f8da06fe53c93487f9ae0348
Stephan Classen <st.classen@gmx.ch>
2016-04-22 22:53:54 +0200

    test variations of quotes in substitution of attributes

--- src/test/groovy/org/asciidoctor/extension/ScreenshotMacroSubstitutionTest.groovy
+++ src/test/groovy/org/asciidoctor/extension/ScreenshotMacroSubstitutionTest.groovy
@@ -31,7 +31,6 @@ import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 
 import javax.imageio.ImageIO
-import java.awt.image.BufferedImage
 
 /**
  * Integration test for substituting attributes in teh screenshot macro.
@@ -39,22 +38,32 @@ import java.awt.image.BufferedImage
 class ScreenshotMacroSubstitutionTest extends Specification {
 
     private static final String url = ScreenshotMacroSubstitutionTest.classLoader.getResource("sample.html").toString()
-    private static final String document1 = """ = How to use Google properly
+    private static final String document1 = """= Test substitutions
 
 :url: ${url}
-:name: imgName
-:dimension: 200x300
 :title: With Substitution
+
+:name1: imgName1
+:name2: imgName2
+:name3: imgName3
+:dimension1: 201x301
+:dimension2: 202x302
+:dimension3: 203x303
 
 == Process
 
 .{title}
-screenshot::{url}[name="{name}", dimension='{dimension}']
+screenshot::{url}[name="{name1}", dimension="{dimension1}"]
+
+screenshot::{url}[name='{name2}', dimension='{dimension2}']
+
+screenshot::{url}[name={name3}, dimension={dimension3}]
 
 """
 
     @Rule
     TemporaryFolder tmpFolder = new TemporaryFolder()
+
     private File outputDir
     private Options options
     private Asciidoctor asciidoctor
@@ -73,14 +82,33 @@ screenshot::{url}[name="{name}", dimension='{dimension}']
           html = html.replaceAll('\n', ' ')
 
         then:
-          html =~ /<img src=".*imgName\.png"/
-          html.contains('alt="imgName"')
           html.contains('<div class="title">Figure 1. With Substitution</div>')
 
-          def imageFile = new File(outputDir, 'screenshots/imgName.png')
-          imageFile.exists()
-          def image = ImageIO.read(imageFile)
-          image.height == 300
-          image.width == 200
+          html =~ /<img src=".*imgName1\.png"/
+          html.contains('alt="imgName1"')
+
+          def imageFile1 = new File(outputDir, 'screenshots/imgName1.png')
+          imageFile1.exists()
+          def image1 = ImageIO.read(imageFile1)
+          image1.height == 301
+          image1.width == 201
+
+          html =~ /<img src=".*imgName2\.png"/
+          html.contains('alt="imgName2"')
+
+          def imageFile2 = new File(outputDir, 'screenshots/imgName2.png')
+          imageFile2.exists()
+          def image2 = ImageIO.read(imageFile2)
+          image2.height == 302
+          image2.width == 202
+
+          html =~ /<img src=".*imgName3\.png"/
+          html.contains('alt="imgName3"')
+
+          def imageFile3 = new File(outputDir, 'screenshots/imgName3.png')
+          imageFile3.exists()
+          def image3 = ImageIO.read(imageFile3)
+          image3.height == 303
+          image3.width == 203
     }
 }
